c01b3899c3c5c69e4f6070cc5840dc057c62b9dc
Hubert Kurek <hkurkov@gmail.com>
2017-04-25 17:40:04 +0200

    Created table instead simple string

--- 08-SinglePageApp/view.js
+++ 08-SinglePageApp/view.js
@@ -3,14 +3,47 @@ var view = {
 		// create new data view to be displayed
 		var elem, text, i;
 		this.clear();
+
+		let dataView = document.querySelector("#dataview");
+		
+		let table = document.createElement("table");
+		dataView.appendChild(table);
+		let tableHead = document.createElement("thead");
+		let tableBody = document.createElement("tbody");
+
+		let headCode = document.createElement("th");
+		headCode.innerText = "Code";
+		let headName = document.createElement("th");
+		headName.innerText = "Name";
+		let headRate = document.createElement("th");
+		headRate.innerText = "Rate";
+		let tr = document.createElement("tr");
+
+		tr.appendChild(headCode);
+		tr.appendChild(headName);
+		tr.appendChild(headRate);
+
+		tableHead.appendChild(tr);
+
+		table.appendChild(tableHead);
+
+
 		for (i = 0; i < data.length; i++) {
-			elem = document.createElement("h3");
-			text = document.createTextNode(
-				data[i].code + "/" + data[i].name + " " + data[i].rate
-			);
-			elem.appendChild(text);
-			document.getElementById('dataview').appendChild(elem);
+			let elem = document.createElement("tr");
+
+			let code = document.createElement("td");
+			code.innerText = data[i].code;
+			let name = document.createElement("td");
+			name.innerText = data[i].name;
+			let rate = document.createElement("td");
+			rate.innerText = data[i].rate;
+
+			elem.appendChild(code);
+			elem.appendChild(name);
+			elem.appendChild(rate);
+			tableBody.appendChild(elem);
 		}
+		table.appendChild(tableBody);
 	},
 	clear: function () {
 		// remove old data
