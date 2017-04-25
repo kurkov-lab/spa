33618d4c535f2a855b7361773ba9368c20fba71e
Hubert Kurek <hkurkov@gmail.com>
2017-04-25 17:01:23 +0200

    Added drop-down instead input text

--- 08-SinglePageApp/controller.js
+++ 08-SinglePageApp/controller.js
@@ -1,20 +1,18 @@
 var controller = {
-		getCurrency: function () {
-			return this.model.getData();
-		},
-		storeCurrency: function (currency) {
-			this.model.addData(currency);
-			this.updateView();
-		},
-		delCurrency: function (code) {
-
-		},
-		updateView: function () {
-			this.data = this.getCurrency();
-			this.view.display(this.data);
-		},
-		model: model,
-		view: view,
-		data: null
-	};
-	
+	getCurrency: function () {
+		return this.model.getData();
+	},
+	storeCurrency: function (currency) {
+		this.model.addData(currency);
+		this.updateView();
+	},
+	delCurrency: function (code) {
+	},
+	updateView: function () {
+		this.data = this.getCurrency();
+		this.view.display(this.data);
+	},
+	model: model,
+	view: view,
+	data: null
+};
